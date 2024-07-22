11e8ba78afdb69ea618c5c2b57da881b0ae7fc16
isadhwani <isadhwani16@gmail.com>
2024-07-22 21:07:43 +1000

    initial

--- backend/app/routers/endpoints.py
+++ backend/app/routers/endpoints.py
@@ -1,56 +1,62 @@
-from fastapi import APIRouter, HTTPException, status, Response
-from pydantic import BaseModel, validator
-import base64
-
-from app.services.robinhood_service import RobinhoodService
 import json
+
+from fastapi import APIRouter, HTTPException, status, Response
+from app.services.robinhood_service import RobinhoodService
+from app.account.user import Users
+from app.account.credentials import Credentials
+from app.account.user import User
 
 router = APIRouter()
 
+# Map <userid> to <user>
+users = Users()
+
 provided_API_keys = False
-
-
-
-
-class Credentials(BaseModel):
-    API_KEY: str
-    BASE64_PRIVATE_KEY: str
-
-    @validator('BASE64_PRIVATE_KEY')
-    def validate_base64(cls, v):
-        try:
-            base64.b64decode(v)
-        except Exception:
-            raise ValueError('Invalid base64 string')
-        return v
-
+print("running base code...")
 
 @router.post("/add-credentials", status_code=200)
-def add_credentials(credentials: Credentials,response: Response):
+def add_credentials(credentials: Credentials, response: Response):
     try:
+
         robinhood_service = RobinhoodService(credentials.API_KEY, credentials.BASE64_PRIVATE_KEY)
+        print("ADDED USER!!!")
+        # robinhood_service = RobinhoodService(credentials.API_KEY, credentials.BASE64_PRIVATE_KEY)
     except Exception as e:
         response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+        robinhood_service = None
         return HTTPException(
             #invalid format
             status_code=422, detail = f"Invalid base64 Private Key: {str(e)}"
         )
     holdings = robinhood_service.validate_account()
     if holdings:
-        return {"message": "API Keys are valid", "crypto_holdings": holdings}
+        user_id = users.add_user(credentials, robinhood_service)
+        return {"message": "API Keys are valid", "user_id": user_id, "holdings": holdings}
     else:
+        robinhood_service = None
+        print("1. is robinhood service none? " + str(robinhood_service == None))
         response.status_code = status.HTTP_401_UNAUTHORIZED
         return HTTPException (
             status_code=401, detail=f"Unable to find account with the provided keys."
         )
-    
 
-@router.get("/portfolios")
+
+@router.get("/copy-profiles")
 def fetch_portfolios():
+
     # Load data from JSON file
     with open('profiles/profiles.json', 'r') as f:
         data = json.load(f)
     return data
+
+@router.get("/holdings")
+def fetch_portfolios(user_id: int):
+    print("user size: " + str(len(users.users)))
+
+    user : User = users.get_user(0)
+    return {"holdings" : user.robinhood_service.get_holdings(), "user_id": user_id}
+
+
 
 
 @router.post("/")
