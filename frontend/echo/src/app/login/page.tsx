d46d92bb2813329a630155e656877b7a09f1ceaa
alyssachen04 <alyssachen04@gmail.com>
2024-07-23 16:17:15 +1000

    User Dashboard Page added and linked to
    - still missing page for forgot password but deprioritizing until we have actual accounts
    - still no real validation for forms

--- frontend/echo/src/app/login/page.tsx
+++ frontend/echo/src/app/login/page.tsx
@@ -145,12 +145,14 @@ export default function Home() {
               </div>
 
               <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Log in
-                </button>
+                <Link href="/user_dashboard">
+                  <button
+                    type="submit"
+                    className="flex w-full justify-center rounded-md bg-indigo-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Log in
+                  </button>
+                </Link>
               </div>
             </form>
 
--- frontend/echo/src/app/signin/page.tsx
+++ frontend/echo/src/app/signin/page.tsx
@@ -159,12 +159,14 @@ export default function Home() {
               </div>
 
               <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Sign up
-                </button>
+                <Link href="/user_dashboard"> 
+                  <button
+                    type="submit"
+                    className="flex w-full justify-center rounded-md bg-indigo-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Sign up
+                  </button>
+                </Link>
               </div>
             </form>
           </div>
