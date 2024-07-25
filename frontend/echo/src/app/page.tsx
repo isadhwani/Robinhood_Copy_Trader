070f9a1758a2280466636001c9ed38f74f9dc5bd
isadhwani <isadhwani16@gmail.com>
2024-07-26 09:54:14 +1000

    basic frontend animations

--- frontend/echo/src/app/page.tsx
+++ frontend/echo/src/app/page.tsx
@@ -6,7 +6,18 @@ import Image from "next/image";
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import Link from "next/link";
-import Navigation from '@/components/navigation';
+import logo from '@/images/logo-isaac.png'
+
+  ; import { useEffect, useState } from "react";
+import { animate, motion, useMotionValue, useTransform } from "framer-motion";
+import AnimatedText from "@/components/pages/landing-page/typewriter/animatedText";
+import CursorBlinker from "@/components/pages/landing-page/typewriter/cursorBlinker";
+import Navigation from "@/components/navigation";
+
+
+export interface IAnimTextProps {
+  delay: number;
+}
 
 { /* Navigation Tabs */ }
 const navigation = [
@@ -15,26 +26,48 @@ const navigation = [
 ]
 
 export default function Home() {
+  const delay = 1
+
+  useEffect(() => {
+    const controls = animate(count, baseText.length, {
+      type: "tween",
+      delay: delay,
+      duration: 2,
+      ease: "easeInOut",
+      onComplete: () => {
+        setDone(true);
+      }
+    });
+    return controls.stop;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const [done, setDone] = useState(false);
+  const baseText = "";
+  const count = useMotionValue(0);
+  const rounded = useTransform(count, (latest) => Math.round(latest));
+  const displayText = useTransform(rounded, (latest) =>
+    baseText.slice(0, latest)
+  );
+
   return (
-    <main className="flex min-h-screen flex-col justify-between">
+    // <main className="flex min-h-screen flex-col justify-between">
+    <>
+      <Navigation currentPath={navigation} />
 
-      {/* Navigation */}
-      <Navigation currentPath={navigation}/>
-      
-      {/* Body - items-center TODO: MAKE IMAGE BACKGROUND AND BLUR */ }
+      <div className=" flex flex-col w-full">
 
-      <div className="">
-        <div className="mx-auto">
-            <img
-              alt="Wallpaper of a computer on a blue screen. By Allison Saeng from Upsplash"
-              src="https://images.unsplash.com/photo-1714578187218-d1828f5ebd6e?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            />
-        </div>
-        
-        <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left p-24">
-          This is our HomePage.
+        <div className="flex flex-col items-center">
+          <img alt="logo" src={`https://gcdnb.pbrd.co/images/74MbLqKX2pSi.png?o=1`} />
+          <AnimatedText text="Crypto Like a Pro."/>
+          <CursorBlinker />
+            
+  
         </div>
       </div>
-    </main>
+    </>
+
+
+    // </main >
   );
 }
