55f1c529e758b3029efd5686364808a1cae720cd
jarkin1025 <arkin.ju@northeastern.edu>
2024-07-29 11:43:28 +1000

    changed user dashboard

--- frontend/echo/src/app/user_dashboard/page.tsx
+++ frontend/echo/src/app/user_dashboard/page.tsx
@@ -9,6 +9,7 @@ import Link from "next/link";
 import React from 'react';
 import Navigation from '@/components/navigation';
 import { useState } from 'react';
+import Carousel from '@/components/carousel';
 
 { /* Navigation Tabs */ }
 const navigation = [
@@ -16,7 +17,7 @@ const navigation = [
 ]
 
 export default function Home() {
-  const [accounts, setCopyAccounts] = useState([]);
+  const [accounts, setCopyAccounts] = useState<any[]>([]);
   const [userHoldings, setUserHoldings] = useState(null);
   const [error, setError] = useState('');
   const [selectedProfileId, setSelectedProfileId] = useState('');
@@ -31,7 +32,18 @@ export default function Home() {
         throw new Error('Failed to fetch accounts');
       }
       const data = await response.json();
-      setCopyAccounts(data);
+
+      // Ensure data is an array
+      let accountsArray = [];
+      if (Array.isArray(data)) {
+        accountsArray = data;
+      } else if (data.portfolios && Array.isArray(data.portfolios)) {
+        accountsArray = data.portfolios;
+      } else {
+        throw new Error('Invalid data format');
+      }
+
+      setCopyAccounts(accountsArray);
     }
     catch (error) {
       if (error instanceof Error) {
@@ -94,6 +106,22 @@ export default function Home() {
       setError(error instanceof Error ? error.message : String(error));
     }
   };
+
+
+  const renderPortfolio = (portfolios) => {
+    return (
+      <div key={portfolios.portfolio_id} className="bg-gray-800 p-4 rounded mb-4">
+        <p><strong>ID:</strong> {portfolios.portfolio_id}</p>
+        <p><strong>Name:</strong> {portfolios.data.name}</p>
+        <p><strong>Account Type:</strong> {portfolios.data.type}</p>
+        <img src={portfolios.data.image} alt={portfolios.data.name} className="rounded-full w-36 h-36 object-cover mx-auto" />
+        <p><strong>Percentages:</strong> {JSON.stringify(portfolios.data.percentages)}</p>
+      </div>
+    );
+  };
+
+
+
 
   return (
     <main className="flex min-h-screen flex-col justify-between bg-dark-theme-2">
@@ -181,6 +209,9 @@ export default function Home() {
           </div>
         )}
 
+        <Carousel />
+
+        {/*
         {accounts && (
           <div>
             <h2 className="text-xl mb-2">Accounts:</h2>
@@ -189,9 +220,14 @@ export default function Home() {
             </pre>
           </div>
         )}
+          */}
 
-
-
+          {accounts.length > 0 && (
+            <div>
+              <h2 className="text-xl mb-2">Accounts:</h2>
+              {accounts.map(renderPortfolio)}
+            </div>
+          )}
 
       </div>
     </main>
